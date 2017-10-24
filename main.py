361db58211c77451ef90855ca4ad91e8619f6586
Nando Davis <nandodavis@gmail.com>
2017-10-24 16:43:13 +0200

    flake8 changes

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ except ImportError:
     from bs4 import BeautifulSoup
 
 
-
 SESSION_ID = "e828a14793a34421e7259fa71236bee0"
 API_KEY = "NTkxYjVkMmM4ZDE1MmJhM2EyMGRmOWUxOGY3MjAyMWUzYjgzYjE5ZDBkOGM4MzI4YWQwNDBjNzE5NmU4MGVkZA"
 EVENT_URL = "https://www.ticketswap.nl/event/walibi-halloween-fright-nights/27-oktober/a659e89d-b69f-4cdd-a922-28a643ae0768/453254"
@@ -21,19 +20,19 @@ URL = HOST + "/api/tickets/14c6ccc531/walibi-halloween-fright-nights"
 COOKIES = {"api_access_token": API_KEY, "session": SESSION_ID}
 AMOUNT = 3
 
+
 def explode_ticket(ticket_link):
     """ Gets tokens from ticket page """
     # Get tokens that you need to have to reserve the ticket and getting the get in cart link
     response = requests.get(HOST + ticket_link, cookies=COOKIES)
     parsed_html = BeautifulSoup(response.content.decode("utf-8"), "html.parser")
-
-    token_object = parsed_html.body.find('input', attrs={"name":"token"})
+    token_object = parsed_html.body.find('input', attrs={"name": "token"})
     if token_object is None:
         print("Failed to get token")
         return False
     token_attrs = token_object.attrs
+    reserve_token_object = parsed_html.body.find('input', attrs={"name": "reserve[_token]"})
 
-    reserve_token_object = parsed_html.body.find('input', attrs={"name":"reserve[_token]"})
     if reserve_token_object is None:
         return False
     reserve_token_attrs = reserve_token_object.attrs
@@ -54,12 +53,13 @@ def explode_ticket(ticket_link):
             AMOUNT = AMOUNT - count
     token = token_attrs['value']
     reserve_token = reserve_token_attrs['value']
-    ticket_link_reserve = parsed_html.body.find('form', attrs={"id":"listing-reserve-form"}).attrs
+    ticket_link_reserve = parsed_html.body.find('form', attrs={"id": "listing-reserve-form"}).attrs
     ticket_reserve_link = ticket_link_reserve['data-endpoint']
     return {"token": token,
             "reserve_token": reserve_token,
             "ticket_link": ticket_reserve_link,
             "more_data": add_data}
+
 
 def get_ticket():
     """ Get Cheapest ticket """
@@ -83,7 +83,6 @@ def get_ticket():
     return False
 
 
-
 def reserve_ticket():
     """ Reserve ticket """
     content = get_ticket()
@@ -100,7 +99,7 @@ def reserve_ticket():
 
 
 if __name__ == "__main__":
-    if  len(sys.argv) >= 2 and sys.argv[1] is not None:
+    if len(sys.argv) >= 2 and sys.argv[1] is not None:
         EVENT_URL = sys.argv[1]
     if len(sys.argv) >= 3 and sys.argv[2] is not None:
         SESSION_ID = sys.argv[2]
