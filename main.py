c76ef3af32f92231ef263a9e27e20932f6ca8e38
ariachy <anthony.riachy@gmail.com>
2019-03-27 20:45:35 +0300

    Update main.py
    Minor bug appeared when ticketswap login modal shows the web version of the plugin. The id of the login button is different. The if elif statement should solve this issue.

--- main.py
+++ main.py
@@ -43,6 +43,18 @@ class TicketSwap:
 
         userInput.send_keys(username)
         passInput.send_keys(password)
+        
+        if driver.find_element_by_id('loginbutton'):
+                send_login = driver.find_element_by_id('loginbutton')
+                send_login.click()
+
+                time.sleep(1)
+        
+        elif driver.find_element_by_id('u_0_0'):
+                send_login = driver.find_element_by_id('u_0_0')
+                send_login.click()
+
+                time.sleep(1)
 
         send_login = driver.find_element_by_id('u_0_0')
         send_login.click()
